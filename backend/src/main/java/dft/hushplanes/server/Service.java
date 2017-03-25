7af430310d801cb940a8a01a99cb656fa9c87ae2
CC <collinschloe14@yahoo.com>
2017-03-25 13:41:20 +0000

    HTTP endpoint  for JSON

--- backend/src/main/java/dft/hushplanes/server/Service.java
+++ backend/src/main/java/dft/hushplanes/server/Service.java
@@ -1,9 +1,43 @@
 package dft.hushplanes.server;
 
-import dft.hushplanes.model.Flights;
+import com.google.gson.Gson;
 
+import java.util.Date;
+
+import dft.hushplanes.model.Flights;
+import dft.hushplanes.parser.AircraftListJsonResponse;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+@Path("/json/aircraft")
 public class Service {
 	Flights get() {
 		return null;
+	}
+
+	@GET
+	@Path("/get/{time}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public AircraftListJsonResponse getAircraftInJSON(@Context UriInfo uriInfo, @QueryParam("time") long searchedTime) {
+
+		AircraftListJsonResponse aircraftAll = new AircraftListJsonResponse();
+		return aircraftAll;
+	}
+	@POST
+	@Path("/post")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response createAircraftInJSON(AircraftListJsonResponse flight) {
+
+		String result = "" + flight;
+		return Response.status(201).entity(result).build();
 	}
 }
